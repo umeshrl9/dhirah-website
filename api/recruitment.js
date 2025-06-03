4e7c66567696ff5fbd9ebb72c33154fda2851c75
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2025-06-03 23:34:04 +0530

    removing deprecated attributes from mongo connection

--- api/recruitment.js
+++ api/recruitment.js
@@ -20,10 +20,7 @@ async function dbConnect() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then(mongoose => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then(mongoose => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
