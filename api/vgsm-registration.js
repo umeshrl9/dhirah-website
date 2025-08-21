abfecdb0ce74930bf16d21b0740cdfb16a55d12f
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2025-08-21 12:21:12 +0530

    added vgsm recruitment form

---
+++ api/vgsm-registration.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then(mongoose => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+// User model
+const VGSMRegistrationSchema = new mongoose.Schema({
+  name: String,
+  rollno: String,
+  year: String,
+  branch: String,
+  phone: String,
+  role: String,
+  skills: String,
+  portfolio: String,
+  motivation: String
+});
+
+const VGSMRegistration = mongoose.models.VGSMRegistration || mongoose.model("VGSMRegistration", VGSMRegistrationSchema);
+
+// Setup in-memory rate limiter
+// const rateLimitOptions = {
+//   max: 5,                  // max 5 requests
+//   ttl: 60 * 1000 * 15,          // per 15 minutes
+// };
+// const rateLimiter = new LRUCache(rateLimitOptions);
+
+//API handler function
+export default async function handler(req, res) {
+    await dbConnect();
+
+    if (req.method === "POST") {
+    try {
+      const { name, rollno, year, branch, phone, role, skills, portfolio, motivation } = req.body;
+
+      const newRegistration = new VGSMRegistration({
+        name,
+        rollno,
+        year,
+        branch,
+        phone,
+        role,
+        skills,
+        portfolio,
+        motivation
+      });
+
+      await newRegistration.save();
+
+      res.writeHead(302, { Location: "/success" });
+      res.end();
+    } catch (error) {
+      console.error(error);
+      res.writeHead(302, { Location: "/error" });
+      res.end();
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
