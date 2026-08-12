df2d8ae8efa70fe69e66f1886a655f5ee7232f4b
Umesh Kashyap <umesh@Umeshs-MacBook-Air.local>
2026-08-12 13:23:27 +0530

    test commit

--- api/mind-your-mind-registration.js
+++ api/mind-your-mind-registration.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+console.log("--> CURRENT MONGO URI:", process.env.MONGODB_URI);
+
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
